3e1ea2ef4baae5f136d0df33c6e52fa5311ffe1a
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-20 14:17:56 +0300

    fetch cart items from the pinia store and show "cart empty" message if there is no items in the cart

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -3,12 +3,7 @@ import {defineStore} from "pinia";
 export const useCartStore = defineStore('cartStore', {
     state: () => {
         return {
-            items: [
-                {
-                    count: 0,
-                    product: {}
-                }
-            ]
+            items: []
         }
     }
 })
