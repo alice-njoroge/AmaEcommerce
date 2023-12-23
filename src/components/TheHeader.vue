73351669de4978b3f0342cc6105e065838464ce1
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-23 11:36:38 +0300

    using pinia in options API -  Refactor TheHeader.vue to include AuthStore
    TheHeader.vue has been updated to include AuthStore usage. The user's data is now neatly displayed in between store imports and the CartWidget. This allows for a smoother and more unified user experience by consolidating state management.

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,29 @@
-<script setup>
+<script>
 // imports
 import CartWidget from './CartWidget.vue';
+import {useAuthStore} from "@/stores/AuthStore";
+import {mapState} from "pinia";
+
+
+export default {
+  components: {CartWidget},
+  computed: {
+    ...mapState(useAuthStore, ["user"])
+
+  }
+
+}
 </script>
 
 <template>
-  <header 
-    class="flex justify-between p-6 mb-10 items-center"
-    style="background-image: url('/images/double-bubble-outline.png')"
+  <header
+      class="flex justify-between p-6 mb-10 items-center"
+      style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div class="flex justify-between gap-5 font-bold">
+      <span>{{user}}</span>
+      <CartWidget/>
+    </div>
   </header>
 </template>
