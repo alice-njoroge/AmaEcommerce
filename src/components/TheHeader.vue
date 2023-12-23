9e7e8221397c316fc7d44b9834f248e949903111
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-23 11:45:04 +0300

    Update usage from mapState to mapWritableState in TheHeader.vue
    Changed the import in TheHeader.vue component to mapWritableState from the previously used mapState. Also, added a binding to a user input element to allow manipulation of the output before it gets rendered in the UI.

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,13 +2,13 @@
 // imports
 import CartWidget from './CartWidget.vue';
 import {useAuthStore} from "@/stores/AuthStore";
-import {mapState} from "pinia";
+import { mapWritableState} from "pinia";
 
 
 export default {
   components: {CartWidget},
   computed: {
-    ...mapState(useAuthStore, {
+    ...mapWritableState(useAuthStore, {
       user: store => `hello ${store.user}`
     })
 
@@ -24,6 +24,7 @@ export default {
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
     <div class="flex justify-between gap-5 font-bold">
+      <input v-model="user" type="text" >
       <span>{{user}}</span>
       <CartWidget/>
     </div>
