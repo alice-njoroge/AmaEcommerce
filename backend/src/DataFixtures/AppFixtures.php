e4f4a78767545d35906a6194c01c381caf8cb145
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-01-04 18:20:10 +0300

    playing with PHP functions

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -13,13 +13,18 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $generator = Factory::create('en_EN');
-        $images = ['plain-old-pineapple.jpg', 'dried-pineapple.jpg', 'pineapple-gum.jpg', 'pineapple-tshirt.jpg' ];
+        $names = ['Plain Old Pineapple', 'Dried Pineapple', 'Pineapple Gum', 'Pineapple Tshirt' ];
+
+        $images = array_map(
+            function($name) {
+            return strtolower(str_replace(' ', '-', $name)).'.jpg';
+        }, $names);
 
         for ($i = 0; $i < 4; $i++) {
 
             $product = new Product();
 
-            $product->setName($generator->name);
+            $product->setName($generator->randomElement($names));
             $product->setQuantity($generator->numberBetween(1, 20));
             $product->setImageURL($generator->randomElement($images));
             $product->setCreatedAt(new DateTimeImmutable());
