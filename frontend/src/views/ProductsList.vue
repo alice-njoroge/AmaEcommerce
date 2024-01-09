7e2f185b541090b5d838374083fe052eaa00c4dd
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-01-09 12:09:31 +0300

    Refactor CSS styling for cards and products list

--- frontend/src/views/ProductsList.vue
+++ frontend/src/views/ProductsList.vue
@@ -28,7 +28,7 @@ onMounted(() => {
     <div class="flex justify-end">
       <app-button  class="secondary mb-2" @click=handleClick> Create New </app-button>
     </div>
-    <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+    <ul class="flex flex-wrap justify-center gap-5">
       <ProductCard
           v-for="product in productsStore.products"
           :key="product.name"
