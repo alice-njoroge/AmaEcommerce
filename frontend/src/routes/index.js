c800aa5bdd5622e3f4023484762a93faf56784be
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-01-15 18:33:29 +0300

    Per Route guard - Add authentication check to product list route
    Incorporated an authentication check on the product listing route to confirm if a user is logged in before accessing the page via 'useAuthStore()' function. No significant modifications made to other routes. This update bolsters security by permitting only authenticated users to view the products list.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,15 +1,39 @@
 import {createRouter, createWebHistory} from "vue-router";
 import ProductsList from "@/views/ProductsList.vue";
+import {useAuthStore} from "@/stores/AuthStore";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path:'/', name:'ProductsList', component:ProductsList},
-        {path:'/create', name:'ProductsCreate', component:()=>import('@/views/ProductsCreate.vue')},
-        {path:'/register', name:'RegisterUser', component:()=> import('@/views/Auth/RegisterUser.vue')},
-        {path:'/login', name:'LoginUser', component:()=> import('@/views/Auth/LoginUser.vue')},
+        {
+            path: '/',
+            name: 'ProductsList',
+            component: ProductsList,
+            beforeEnter: (to, from) => {
+                const authenticated = () => useAuthStore().isLoggedIn;
+                if (!authenticated()) {
+                    return {name: 'LoginUser'}
+                }
+            }
+        },
+        {
+            path: '/create',
+            name: 'ProductsCreate',
+            component: () => import('@/views/ProductsCreate.vue')
+        },
+        {
+            path: '/register',
+            name: 'RegisterUser',
+            component: () => import('@/views/Auth/RegisterUser.vue')
+        },
+        {
+            path: '/login',
+            name: 'LoginUser',
+            component: () => import('@/views/Auth/LoginUser.vue')
+        },
     ],
     linkActiveClass: 'active'
 })
+
 
 export default router;
