d635f3a9172b5b6908d2f2fa4457ea7784a53fb1
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-02-06 20:37:02 +0300

    updated make file to include new commands and added tailwind CSS to admin page for the products table

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 .PHONY: up
 up:
 	docker compose up -d
+	docker compose logs -f
 
 .PHONY: down
 down:
@@ -23,3 +24,15 @@ reset-database:
 .PHONY: cs-fix #phony name
 cs-fix: # actual command name that will be ran on terminal
 	docker compose exec back composer run cs-fix #the long command that you are shortening
+
+.PHONY: blogs
+blogs:
+	docker compose exec back tail -f /var/www/html/var/log/dev.log
+
+.PHONY: bbash
+bbash:
+	docker compose exec back bash
+
+.PHONY: wbash
+wbash:
+	docker compose exec webApp bash
--- UI/src/pages/admin/index.vue
+++ UI/src/pages/admin/index.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import {useAdminProducts} from "~/stores/adminProducts";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
+console.log(faPenToSquare)
 const adminProducts = useAdminProducts();
 onMounted(() => {
   adminProducts.getProducts();
@@ -9,33 +12,42 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="col-md-8">
-    <h1 class="text-black-700 text-center">Products</h1>
-    <table class="table-fixed w-full text-left">
-      <thead>
-      <tr>
-        <th>Product Name</th>
-        <th>Image URL</th>
-        <th>Quantity</th>
-        <th>Price</th>
-      </tr>
-      </thead>
-      <tbody class="text-left">
-      <tr v-for="products in adminProducts.products">
-        <td>{{ products.name }}</td>
-        <td>
-          <NuxtLink to="/">
-            {{ products.imageURL }}
-          </NuxtLink>
-        </td>
-        <td> {{products.quantity}} </td>
-        <td> {{products.price}} </td>
 
-      </tr>
-      </tbody>
-    </table>
-  </div>
 
+  <h1 class="text-center dark:text-black-700 font-bold ">Products</h1>
+
+
+    <div class="card-table relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left rtl:text-right text-black-500 ">
+        <thead class="text-left text-green-700 uppercase border-b dark:border-gray-700">
+        <tr class="font-bold text-black-900">
+          <th scope="col" class="pl-2 py-7">Product Name</th>
+          <th scope="col" class="py-7">Image URL</th>
+          <th scope="col" class="py-7">Quantity</th>
+          <th scope="col" class="py-7">Price</th>
+          <th scope="col" class="py-7">Actions</th>
+        </tr>
+        </thead>
+        <tbody class="text-left">
+        <tr class="bg-white border-b dark:border-gray-300" v-for="products in adminProducts.products">
+          <td class="px-6 py-4">{{ products.name }}</td>
+          <td>
+            <NuxtLink to="/">
+              {{ products.imageURL }}
+            </NuxtLink>
+          </td>
+          <td> {{ products.quantity }}</td>
+          <td> {{ products.price }}</td>
+          <td>
+            <font-awesome-icon :icon="faPenToSquare" class="mr-4" />
+            <font-awesome-icon :icon="faTrashCan" />
+          </td>
+
+        </tr>
+        </tbody>
+      </table>
+
+    </div>
 </template>
 
 <style scoped>
